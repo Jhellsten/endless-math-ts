26896792f62fc35ec8461a8431552506b20fab33
Janne Hellsten <hellsten@live.com>
2021-06-13 01:56:16 +0300

    feat: added dimension and color variables

---
+++ src/shared/theme/variables/colors.ts
@@ -0,0 +1,7 @@
+// Color naming rule: It should be name of color for readability.
+export default {
+    white: '#F5FCFF',
+    red: '#ff3333',
+    blue: '#0066ff',
+    black: '#000000'
+}
---
+++ src/shared/theme/variables/dimensions.ts
@@ -0,0 +1,26 @@
+export default {
+    fontSize: {
+        medium: 20,
+        big: 30,
+        huge: 40
+    },
+    margin: {
+        medium: 10,
+        big: 20
+    },
+    padding: {
+        small: 6,
+        medium: 10
+    },
+    input: {
+        height: 50
+    },
+    button: {
+        height: 50
+    },
+    border: {
+        small: 0.5,
+        medium: 1,
+        smallRadius: 4
+    }
+}
