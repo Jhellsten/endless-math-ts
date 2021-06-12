2dd10119c5c82e43a15b95bbb6e7f3a6a371dd91
Janne Hellsten <hellsten@live.com>
2021-06-13 01:30:08 +0300

    feat: added logo and header images, added custom font, updated react native config

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   assets: [
     "./src/assets/fonts",
+    "./src/assets/images",
+    "./src/assets/fonts/PhosphateInline/PhosphateInline.ttf",
     "./src/assets/fonts/SeuzOne/SuezOne-Regular.ttf",
   ],
 };
