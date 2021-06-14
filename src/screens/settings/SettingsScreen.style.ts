f0acc10753b7c6ca7727737d35f646c360449d9d
Janne Hellsten <hellsten@live.com>
2021-06-15 00:37:01 +0300

    feat: added settings screen and styles

---
+++ src/screens/settings/SettingsScreen.style.ts
@@ -0,0 +1,39 @@
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
+import {  colors, dimensions } from '@theme-variables'
+import themes from '@themes'
+import { mixins } from '@mixins'
+
+interface Style {
+    buttonContainerStyle: ViewStyle;
+    containerStyle: ViewStyle;
+    settingRowStyle: ViewStyle;
+    textStyle: TextStyle;
+    textRowStyle: TextStyle;
+  }
+
+export default StyleSheet.create<Style>({
+    buttonContainerStyle: {
+        marginBottom: '5%',
+        width: '100%'
+    },
+    containerStyle: {
+        ...mixins.containerStyle,
+        backgroundColor: colors.white
+    },
+    settingRowStyle: {
+        flexDirection: 'row'
+    },
+    textStyle: {
+        ...mixins.mediumText,
+        color: themes.mint.secondary,
+        margin: '10%',
+        textAlign: 'center'
+    },
+    textRowStyle: {
+        color: themes.mint.secondary,
+        margin: dimensions.margin.medium,
+        minWidth: '20%',
+        padding: dimensions.padding.medium,
+        textAlign: 'center'
+    }
+})
---
+++ src/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import * as NavigationService from "react-navigation-helpers";
+import { SettingsStateType } from 'reducers/settings'
+import { SETTINGS_OPTIONS } from '@shared-constants';
+import themes, { ThemeType } from '@themes'
+
+import styles from "./SettingsScreen.style"
+import CustomInput from '@shared-components/CustomInput/CustomInput';
+import Button from '@shared-components/Button/Button';
+import { Setting, Theme } from '@actions';
+import Header from '@shared-components/Header/Header';
+
+interface IProps {
+    themes: typeof themes,
+    settings: any,
+    changeSetting: (text: string, value: string) => void,
+    changeTheme: (theme: ThemeType) => void
+  }
+  
+  interface IState {}
+
+class Settings extends Component<IProps, IState> {
+
+    _renderSettings() {
+        return Object.keys(SETTINGS_OPTIONS).map((setting, index) => {
+            return (
+                <View style={styles.settingRowStyle} key={index}>
+                    <Text style={styles.textRowStyle}>{Object.values(SETTINGS_OPTIONS)[index]}</Text>
+                    <CustomInput
+                        width={'30%'}
+                        value={this.props.settings[setting]}
+                        keyboardType={'number-pad'}
+                        onChangeText={(text: string) => this.props.changeSetting(setting, text)}
+                    ></CustomInput>
+                </View>
+            )
+        })
+    }
+    _renderThemeSelection() {
+        const { themes, changeTheme } = this.props
+        console.log(themes)
+        return Object.keys(themes).map((theme: keyof typeof themes, index: number) => {
+            return (
+                <View key={index}>
+                    {/* <Text style={styles.textRow}>{theme}</Text> */}
+                    <Button
+                        width={'30%'}
+                        style={{
+                            ...styles.textRowStyle,
+                            backgroundColor: themes[theme].primary
+                        }}
+                        textColor={themes[theme].secondary}
+                        text={themes[theme].name}
+                        // value={this.props.settings[theme]}
+                        onPress={() => changeTheme(themes[theme])}
+                    ></Button>
+                </View>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.containerStyle}>
+                <Header
+                    goBack={() => NavigationService.goBack()}
+                    back={true}
+                    // title={'Asetukset'}
+                    // titleColor={colors.red}
+                />
+                <KeyboardAwareScrollView>
+                    <Text style={styles.textStyle}>Asetukset</Text>
+                    {this._renderSettings()}
+                    <Text style={styles.textStyle}>Teema</Text>
+                    {this._renderThemeSelection()}
+                </KeyboardAwareScrollView>
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = (state: {settings: SettingsStateType}) => ({
+    settings: state.settings.settings,
+    themes: state.settings.selectableThemes
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    changeSetting: (name: string, value: string) => dispatch(Setting.changeSetting(name, value)),
+    changeTheme: (theme: ThemeType) => dispatch(Theme.changeTheme(theme))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings)
