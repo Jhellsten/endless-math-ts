a3114bbe830ec37008c983b132d524dd156bbac0
Janne Hellsten <hellsten@live.com>
2021-06-18 01:11:16 +0300

    fix: separeted calculate, updated calculate function

--- src/utils/calculators.ts
+++ src/utils/calculators.ts
@@ -1,23 +1,44 @@
-export const calculate = (op: string, number1: number, number2: number, number3: number): number => {
-    switch (op) {
-        case '+':
-            return number3 ? number1 + number2 + number3 : number1 + number2
-        case '-':
-            return number3 ? number1 - number2 - number3 : number1 - number2
-        case '*':
-            return number3 ? number1 * number2 * number3 : number1 * number2
-        case '/':
-            return number3 ? number1 / number2 / number3 : number1 / number2
-        default:
-            return 0
+import { GameOperators } from 'shared/types/game.type'
+
+export function calculate(operator: GameOperators, number1: number, number2: number): number
+export function calculate(operator: GameOperators, number1: number, number2: number, number3: number): number
+export function calculate (operator: GameOperators, number1: number, number2: number, number3?: number): number {
+    if(number3) {
+        switch (operator) {
+            case GameOperators.addition:
+                return number1 + number2 + number3
+            case GameOperators.substraction:
+                return number1 - number2 - number3
+            case GameOperators.multiply:
+                return number1 * number2 * number3
+            case GameOperators.division:
+                return number1 / number2 / number3
+            default:
+                return 0
+        }
+    } else {
+        switch (operator) {
+            case GameOperators.addition:
+                return number1 + number2
+            case GameOperators.substraction:
+                return number1 - number2
+            case GameOperators.multiply:
+                return number1 * number2
+            case GameOperators.division:
+                return number1 / number2
+            default:
+                return 0
+        }
     }
+   
+    
 }
 
-export const compareNumbers = (operator: string, num1: number, num2: number): boolean => {
+export const compareNumbers = (operator: GameOperators, num1: number, num2: number): boolean => {
     switch (operator) {
-        case '<':
+        case GameOperators.smallerThan:
             return num1 < num2
-        case '>':
+        case GameOperators.largerThan:
             return num1 > num2
         default:
             return false
