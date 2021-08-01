ee14b2313d7d06d840fae18939a7e3e9a25999cb
Janne Hellsten <hellsten@live.com>
2021-08-01 11:02:11 +0300

    feat: added translation strings

--- src/shared/localization/index.ts
+++ src/shared/localization/index.ts
@@ -2,25 +2,75 @@ import LocalizedStrings from "react-native-localization";
 
 export const localStrings = new LocalizedStrings({
   fi: {
-    noLocationService: "Lokasyon Servisi Kapalı",
-    logout: "Kirjaudu ulos",
+    settings: {
+      title: "Asetukset",
+      numberOfRows: "Rivien määrä",
+      theme: "Teema",
+      language: "Kieli",
+      changeLanguage: "Change language to "
+    },
+    en: 'English',
+    fi: 'Suomi',
+    startPlaying: "Aloita pelaaminen",
     yes: "Kyllä",
     no: "Ei",
     cancel: "Peruuta",
+    previous: "Edellinen",
     areYouSure: "Oletko varma?",
     logoutDesc: "Haluatko kirjautua ulos?",
     noInternet: "Ei internet yhteyttä",
+    games: {
+      selectGame: "Valitse peli",
+      addition: "Yhteenlaskut",
+      substraction: "Vähennyslaskut",
+      multiply: "Kertolaskut",
+      multiplyTables: "Kertotaulut",
+      multiplyTable: "ertotaulu",
+      division: "Jakolaskut",
+      picturePuzzles: "Kuva arvoitukset",
+      difficultyLevels: {
+        easy: "Helpot",
+        regular: "Tavalliset",
+        hard: "Vaikeat",
+        impossible: "Mahdottomat"
+      }
+    }
   },
   en: {
-    noLocationService: "Location Service Not Available",
-    logout: "Logout",
+    settings: {
+      title: "Settings",
+      numberOfRows: "Amount of rows",
+      theme: "Theme",
+      language: "Language",
+      changeLanguage: "Vaihda kieli "
+    },
+    en: 'English',
+    fi: 'Suomi',
+    startPlaying: "Start playing",
     yes: "Yes",
     no: "No",
     cancel: "Cancel",
+    previous: "Previous",
     areYouSure: "Are you sure?",
     logoutDesc: "You are about to log out, confirm?",
     noInternet: "No Internet Connection",
+    games: {
+      selectGame: "Choose game",
+      addition: "Addition",
+      substraction: "Subtraction",
+      multiply: "Multiply",
+      multiplyTables: "Multiplication tables",
+      multiplyTable: "multiplication table",
+      division: "Division",
+      picturePuzzles: "Picture puzzles",
+      difficultyLevels: {
+        easy: "Easy",
+        regular: "Regular",
+        hard: "Hard",
+        impossible: "Impossible"
+      }
+    }
   },
 });
 // ? Set the language manually
-localStrings.setLanguage("fi");
+localStrings.setLanguage("en");
